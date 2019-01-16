4605194de5a455b007036c0fad3a129c0716eacc
macreigh <46752570+macreigh@users.noreply.github.com>
2019-01-16 09:49:31 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,26 @@
+/********************************************
+ * Hello
+ ********************************************
+ * Lab 1/Introduction to Java
+ * __________________________________________
+ * Molly Creighton
+ * 16 Jan 2019
+ * CMSC 255 003
+ ********************************************/
+ 
+ public class Hello {
+  public static void printHeading() {
+   System.out.println("Molly");
+   System.out.println("Lab 1");
+   System.out.println("Introduction to Java");
+   System.out.println("20190116");
+   System.out.println("CMSC 255 003");
+   System.out.println();
+  }
+  
+  public static void main(String [] args) {
+   printHeading();
+   System.out.println("Hello");
+  }
+ }
+   
