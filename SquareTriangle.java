3daefd7b2735f276aee46f24c6d0476013fa594c
M Creighton <46752570+macreigh@users.noreply.github.com>
2019-01-23 10:24:47 -0500

    Add files via upload

---
+++ SquareTriangle.java
@@ -0,0 +1,15 @@
+public class SquareTriangle
+{
+	public static void main(String [] args) {
+		System.out.println("# # # # # # # # # #");
+		System.out.println("#        ##       #");
+		System.out.println("#       #  #      #");
+		System.out.println("#      #    #     #");
+		System.out.println("#     #      #    #");
+		System.out.println("#    #        #   #");
+		System.out.println("#   #          #  #");
+		System.out.println("#  #            # #");
+		System.out.println("# # # # # # # # # #");
+		
+	}
+}
