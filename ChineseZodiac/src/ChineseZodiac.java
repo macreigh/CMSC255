d61d404a987d4822874e6a92b11524908902d332
aurandkr <46753101+aurandkr@users.noreply.github.com>
2019-03-26 19:04:16 -0400

    Working GUI and output
    At the moment it only outputs the text, no picture, and no description. I copied the text line from milestone2. I did a ton of reworking because I was having a really hard time trying to keep up and read the code. I ended up starting from scratch adding in the methods from the original code as I got to their specific segments. What's left is : image upload, description addition, error handling and I think that's it.

--- ChineseZodiac/src/ChineseZodiac.java
+++ ChineseZodiac/src/ChineseZodiac.java
@@ -1,29 +1,21 @@
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.GridLayout;
-import java.awt.Button;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
-import javax.swing.JTextPane;
 import java.awt.Color;
 import java.awt.Font;
-import javax.swing.JList;
 
-public class ChineseZodiac extends JFrame {
-
-	private JPanel contentPane;
-	private JTextField userInput;
-
-	/**
-	 * Launch the application.
-	 */
+public class ChineseZodiac extends JFrame{
+	
+	private JPanel inputPanel;
+	private JTextField userYear;
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -36,42 +28,65 @@ public class ChineseZodiac extends JFrame {
 			}
 		});
 	}
-
-	/**
-	 * Create the frame.
-	 */
+	
+	/***************************
+	 * This class build a GUI for our task.
+	 ***************************/
 	public ChineseZodiac() {
-		setTitle("Chinese Zodiac Calculator");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 648, 535);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		
+		setTitle("Chinese Zodiac Calculator");	//Sets title at top of window.
+		setBounds(100, 100, 648, 535);			//Sets size of frame.
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Sets exit operation for frame.
+		
+		JLabel zodiacText = new JLabel();			//zodiacText is what the program outputs.
+		zodiacText.setBounds(21, 140, 578, 273);	//Use for testing purposes, until we understand how to import images.
+		
+		inputPanel = new JPanel();			//This panel is for the face of the GUI, not including output.
+		inputPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		inputPanel.setLayout(null);
+		
+		JLabel userPrompt = new JLabel();	//userPrompt are the instructions for the user.
+		userPrompt.setBounds(21, 21, 578, 35);
+		userPrompt.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		userPrompt.setForeground(Color.BLACK);
+		userPrompt.setBackground(Color.WHITE);
+		userPrompt.setText("Please enter your birth year below to receive your Zodiac result.");
+		
+		userYear = new JTextField();		//userYear is the year the user inputs.
+		userYear.setBounds(21, 70, 431, 32);
+		userYear.setColumns(10);
 		
 		JButton submitButton = new JButton("Submit");
+		submitButton.setBounds(458, 69, 141, 35);
 		submitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				int year = Integer.parseInt(userYear.getText());	//Stores the string from JTextField as an int. NEEDS ERROR HANDLING.
+				zodiacText.setText(zodiacSign(year));				//Stores the returned String from method zodiacYear() into zodiacText.
 			}
 		});
-		submitButton.setBounds(458, 69, 141, 35);
-		contentPane.add(submitButton);
 		
-		userInput = new JTextField();
-		userInput.setBounds(21, 70, 431, 32);
-		contentPane.add(userInput);
-		userInput.setColumns(10);
+		inputPanel.add(userPrompt);			//Adds values to inputPanel.
+		inputPanel.add(userYear);
+		inputPanel.add(submitButton);
 		
-		JLabel userInstructions = new JLabel();
-		userInstructions.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		userInstructions.setForeground(Color.BLACK);
-		userInstructions.setBackground(Color.WHITE);
-		userInstructions.setText("Please enter your birth year below to receive your Zodiac result.");
-		userInstructions.setBounds(21, 21, 578, 35);
-		contentPane.add(userInstructions);
+		add(zodiacText);		//Adds values to frame.
+		add(inputPanel);
+		setVisible(true);
 		
-		JPanel resultBox = new JPanel();
-		resultBox.setBounds(21, 140, 578, 273);
-		contentPane.add(resultBox);
+	}
+	
+	/***************************************
+	 * This method is called with an integer parameter and returns
+	 * the user's zodiac sign as an index of a string array 
+	 * corresponding to the user's input year.
+	 ***************************************/
+	public static String zodiacSign(int year) {
+		String zodiac;
+		int sign = year % 12;
+		String[] animals = { "Monkey", "Rooster", "Dog", "Boar", "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake",
+				"Horse", "Ram" };
+		zodiac = "According to the Chinese New Year, your zodiac sign is the " + animals[sign];
+
+		return zodiac;
 	}
 }
