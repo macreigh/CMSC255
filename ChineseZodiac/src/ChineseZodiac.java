43d016c139d02b0bc669efd9cda275243bc1773c
aurandkr <46753101+aurandkr@users.noreply.github.com>
2019-04-10 17:40:22 -0400

    Update ChineseZodiac.java

--- ChineseZodiac/src/ChineseZodiac.java
+++ ChineseZodiac/src/ChineseZodiac.java
@@ -3,6 +3,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import java.awt.event.ActionListener;
@@ -10,12 +11,19 @@ import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Image;
 
 public class ChineseZodiac extends JFrame{
 	
+	/*****************************************************
+	 * Here we declare a few variables we expect to use throughout
+	 * the program. We also check to see if we are allowed to build
+	 * a new frame for our project.
+	 ****************************************************/
 	private JPanel inputPanel;
 	private JTextField userYear;
-	
+	private GridLayout grid;
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -29,75 +37,126 @@ public class ChineseZodiac extends JFrame{
 		});
 	}
 	
-	/***************************
-	 * This class build a GUI for our task.
-	 ***************************/
+	/***************************************************
+	 * This is the class that builds the GUI for our specific task.
+	 ***************************************************/
 	public ChineseZodiac() {
+		grid = new GridLayout(2,0);
 		
-		setTitle("Chinese Zodiac Calculator");	//Sets title at top of window.
-		setBounds(100, 100, 648, 535);			//Sets size of frame.
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Sets exit operation for frame.
+		/*****************************************************
+		 * Here we set the properties of our Frame(GUI).
+		 ****************************************************/
+		setTitle("Chinese Zodiac Calculator");
+		setBounds(100, 100, 900, 600);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JLabel zodiacText = new JLabel();			//zodiacText is what the program outputs.
-		zodiacText.setBounds(21, 140, 578, 273);	//Use for testing purposes, until we understand how to import images.
+		/*****************************************************
+		 * Here we add and set the properties of a JLabel, it is used 
+		 * for our output, showing errors or showing your zodiac sign
+		 * along with a short text describing it.
+		 ****************************************************/
+		JLabel zodiacText = new JLabel();
+		zodiacText.setBounds(20, 10, 900, 600);
+		zodiacText.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		
-		inputPanel = new JPanel();			//This panel is for the face of the GUI, not including output.
+		/*****************************************************
+		 * Here we declare and set the properties of a JPanel, 
+		 * we use this to prompt the user with our project's objective
+		 * and to accept input from the user using attachments.
+		 * This JPanel is the basic face to our GUI.
+		 ****************************************************/
+		inputPanel = new JPanel();
 		inputPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		inputPanel.setLayout(null);
-		
-		JLabel userPrompt = new JLabel();	//userPrompt are the instructions for the user.
-		userPrompt.setBounds(21, 21, 578, 35);
+		grid.addLayoutComponent("inputpanel", inputPanel);
+		JLabel userPrompt = new JLabel();
+		userPrompt.setBounds(150, 20, 578, 35);
 		userPrompt.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		userPrompt.setForeground(Color.BLACK);
 		userPrompt.setBackground(Color.WHITE);
 		userPrompt.setText("Please enter your birth year below to receive your Zodiac result.");
 		
-		userYear = new JTextField();		//userYear is the year the user inputs.
-		userYear.setBounds(21, 70, 431, 32);
-		userYear.setColumns(10);
+		/*****************************************************
+		 * Here we declare a JTextField which allows the user to 
+		 * insert input for our project.
+		 ****************************************************/
+		userYear = new JTextField();
+		userYear.setBounds(145, 70, 431, 32);
+		userYear.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		userYear.setColumns(5);
 		
+		/*****************************************************
+		 * Here we declare and set the properties of a JButton 
+		 * named "submitButton" and this allows the user to 
+		 * submit their input.
+		 ****************************************************/
 		JButton submitButton = new JButton("Submit");
-		submitButton.setBounds(458, 69, 141, 35);
+		submitButton.setBounds(588, 69, 141, 35);
+		submitButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		
+		/*****************************************************
+		 * Here is where the magic happens, this is where the program
+		 * makes the button have a reaction. Once the button is pushed
+		 * this part of the program runs. First we set all values of the
+		 * JLabel, zodiacText, to null so if the program is run more than
+		 * once or the program runs into an error, the fields are cleared.
+		 * We then do some error checking making sure that the user's 
+		 * input is a valid number and is in bounds to be a valid year.
+		 * If the input passes, the method zodiacImage is called and stored
+		 * into the JLabel's icon box.
+		 ****************************************************/
 		submitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int year = Integer.parseInt(userYear.getText());	//Stores the string from JTextField as an int. NEEDS ERROR HANDLING.
-				int sign = year % 12;
-				zodiacText.setText("According to the Chinese New Year, your zodiac sign is the " + zodiacSign(year));				//Stores the returned String from method zodiacYear() into zodiacText.
-				
-				//Statements pull up new classes in popup window
-				if(sign == 0) {
-					Monkey monkey = new Monkey();
-					monkey.setVisible(true);
+				zodiacText.setIcon(null);
+				zodiacText.setText(null);
+				int year = 0;
+				try {
+					year = Integer.parseInt(userYear.getText());
+					if(year < 10000 && year > 0) {
+						int sign = year % 12;
+						zodiacText.setIcon(zodiacImage(sign));
+						userYear.setText("");
+					}
+					else {
+						year /= 0;
+					}
 				}
-				if(sign == 1) {
-					Rooster rooster = new Rooster();
-					rooster.setVisible(true);
+				catch(NumberFormatException oof) {
+					userYear.setText("");
+					zodiacText.setText("Input invalid, must be an int. Please try again.");
+				}
+				catch(ArithmeticException oof) {
+					userYear.setText("");
+					zodiacText.setText("Input invalid, must be a valid year. Please try again.");
 				}
 			}
 		});
 		
-		inputPanel.add(userPrompt);			//Adds values to inputPanel.
+		/***************************************************
+		 * Adds values to the JPanel, inputPanel.
+		 ***************************************************/
+		inputPanel.add(userPrompt);
 		inputPanel.add(userYear);
 		inputPanel.add(submitButton);
-		
-		add(zodiacText);		//Adds values to frame.
+		/****************************************************
+		 * Adds values to the main frame, ChineseZodiac.
+		 ****************************************************/
+		add(zodiacText);
 		add(inputPanel);
 		setVisible(true);
-		
 	}
 	
 	/***************************************
 	 * This method is called with an integer parameter and returns
-	 * the user's zodiac sign as an index of a string array 
-	 * corresponding to the user's input year.
+	 * the user's zodiac sign as an image taken from a String array
+	 * holding the image directory. The image is specific to the user's
+	 * input year.
 	 ***************************************/
-	public static String zodiacSign(int year) {
-		String zodiac;
-		int sign = year % 12;
-		String[] animals = { "Monkey", "Rooster", "Dog", "Boar", "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake",
-				"Horse", "Ram" };
-		zodiac = animals[sign];
-
-		return zodiac;
+	public static ImageIcon zodiacImage(int sign) {
+		String[] path = {"monkey.png", "rooster.png", "dog.png", "boar.png", "rat.png", "ox.png", "tiger.png", "rabbit.png", "dragon.png", "snake.png", "horse.png", "ram.png"};
+		
+		ImageIcon zodiacImage = new ImageIcon(new ImageIcon(path[sign]).getImage().getScaledInstance(830, 300, Image.SCALE_DEFAULT));
+		
+		return zodiacImage;
 	}
 }
